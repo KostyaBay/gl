1bc42778b0ff971b0a02a606b87a099590aa0cd1
Konstantin <konstantin.bay228@gmail.com>
2021-02-20 03:16:18 +0200

    fixed 3

--- test_list.py
+++ test_list.py
@@ -13,7 +13,7 @@ class TestListMethods(unittest.TestCase):
 
     def test_illia_kripaka_fi_94_2(self):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
-   
+
     def test_kostiantyn_baievskyi_fi_93(self):
         list_1 = [1, 2, 3]
         list_2 = [4, 5, 6]
