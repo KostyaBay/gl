3a621055eef9dd197f19566b1974be81b2c509ff
Konstantin <konstantin.bay228@gmail.com>
2021-02-20 03:09:14 +0200

    fixed 2

--- test_list.py
+++ test_list.py
@@ -15,9 +15,9 @@ class TestListMethods(unittest.TestCase):
         self.assertEqual(2*[1, 3, 5], [1, 3, 5, 1, 3, 5])
    
     def test_kostiantyn_baievskyi_fi_93(self):
-        list_1 = [1,2,3]
-        list_2 = [4,5,6]
-        list_3 = [1,2,3,4,5,6]
+        list_1 = [1, 2, 3]
+        list_2 = [4, 5, 6]
+        list_3 = [1, 2, 3, 4, 5, 6]
         self.assertEqual(list_1+list_2, list_3)
 
 if __name__ == '__main__':
